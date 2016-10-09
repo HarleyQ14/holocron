babca0e8e8cf5b170e6d8cd5424c9b0fad51eb88
Kristen Dergousoff <harleyq14@gmail.com>
2016-10-09 17:02:42 -0600

    Created a quick game to test how best to package the builds so they can be downloaded from git and and ran for testing.

---
+++ src/HelloWorld/Assets/_scripts/Player/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int count;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("escape")) {
+			Application.Quit();
+		}
+	}
+
+	// Updates the physics oncer per frame
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Collectible")) {
+			other.gameObject.SetActive(false);
+			count += 1;
+			SetCountText();
+		}
+	}
+
+	void SetCountText() {
+		countText.text = "Score: " + count.ToString ();
+
+		if (count >= 12) {
+			winText.text = "You win!";
+		}
+	}
+}
